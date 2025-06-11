c91f32887741fc86abd9a64576d49969f1a4f242
sjh <sjh6226@naver.com>
2025-06-11 15:59:59 +0900

    코드 주석 추가

--- cardGame/js/getCardFaceImage.js
+++ cardGame/js/getCardFaceImage.js
@@ -1,6 +1,29 @@
+// 더 간단하게 이미지 불러오는 함수
+
 // 카드 번호에 맞는 앞면 이미지를 반환하는 함수
 export function getCardFaceImage(cardNumber) {
     const img = new Image();
     img.src = `img/${cardNumber}.png`;
     return img;
 }
+
+// 기존에 이미지 불러오는 함수였던 코드
+
+/* // 카드 번호에 맞는 앞면 이미지를 반환하는 함수
+const cardImageMap = {
+    1: '1.png',
+    2: '2.png',
+    3: '3.png',
+    4: '4.png',
+    5: '5.png',
+    6: '6.png',
+    7: '7.png',
+    8: '8.png'
+    // 필요한 만큼 추가
+};
+
+export function getCardFaceImage(cardNumber) {
+    const img = new Image();
+    img.src = `img/${cardImageMap[cardNumber]}`;
+    return img;
+} */
