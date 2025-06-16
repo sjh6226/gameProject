4f8426f97b32b3bd19bc0b3a0970c90ffd6fef8c
sjh <sjh6226@naver.com>
2025-06-16 14:23:32 +0900

    리듬게임 코드 구조 설명 페이지 이미지추가

--- rhythmGame/js/main.js
+++
@@ -1,6 +0,0 @@
-// main.js: RhythmGame 인스턴스 생성 및 게임 시작 함수(window.gameStart) 연결
-
-import { RhythmGame } from './RhythmGame.js';
-
-const rhythmGame = new RhythmGame();
-window.gameStart = () => rhythmGame.gameStart();
